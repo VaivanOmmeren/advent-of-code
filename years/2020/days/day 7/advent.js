9c06f9cc7a95cbcb3c12618e9b0f87f5651a27fb
Vai van Ommeren <vaivanommeren+1@gmail.com>
2020-12-07 14:10:55 +0100

    feature: day 7 part one complete

--- years/2020/days/day 7/advent.js
+++ years/2020/days/day 7/advent.js
@@ -1,12 +1,42 @@
 const fs = require("fs");
 const path = require("path");
-
+let canContainCount = 0;
+const rulesObject = {}
+const haveChecked = []
 
 function partOne (pathString) {
     const input = fs.readFileSync(path.join(__dirname, pathString))
     const rules = input.toString().split(new RegExp(/\n/, "g"))
+    const myBag = "shiny gold";
 
-    return 4;
+    rules.forEach((rule) => {
+        const splitRule = rule.split("contain")
+        rulesObject[splitRule[0]] = splitRule[1].split(',')
+    })
+
+    findBagThatContainsMe(myBag)
+    console.log(canContainCount)
+    return canContainCount;
+}
+
+function findBagThatContainsMe(name) {
+    if (name != null) {
+        for(const [key, value] of Object.entries(rulesObject)) {
+            value.forEach((bag) => {
+                if(bag.match(new RegExp(name, "g"))) {
+                    const splitKey = key.split(" ").splice(0,2).join(" ")
+
+                    if (!haveChecked.includes(splitKey)) {
+                        haveChecked.push(splitKey)
+                        canContainCount++;
+                        findBagThatContainsMe(splitKey);
+                    }
+                }
+            })
+        }
+    }
+
+    return;
 }
 
 module.exports = {
