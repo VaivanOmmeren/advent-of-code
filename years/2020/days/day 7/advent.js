4dc56f955ca61f77d521bcfce424f8e5919737d6
Vai van Ommeren <vaivanommeren+1@gmail.com>
2020-12-08 10:49:23 +0100

    feature: day 7 part two complete

--- years/2020/days/day 7/advent.js
+++ years/2020/days/day 7/advent.js
@@ -6,7 +6,7 @@ const haveChecked = []
 
 function partOne (pathString) {
     const input = fs.readFileSync(path.join(__dirname, pathString))
-    const rules = input.toString().split(new RegExp(/\n/, "g"))
+    const rules = input.toString().trimEnd().split(new RegExp(/\n/, "g"))
     const myBag = "shiny gold";
 
     rules.forEach((rule) => {
@@ -15,7 +15,6 @@ function partOne (pathString) {
     })
 
     findBagThatContainsMe(myBag)
-    console.log(canContainCount)
     return canContainCount;
 }
 
@@ -25,7 +24,6 @@ function findBagThatContainsMe(name) {
             value.forEach((bag) => {
                 if(bag.match(new RegExp(name, "g"))) {
                     const splitKey = key.split(" ").splice(0,2).join(" ")
-
                     if (!haveChecked.includes(splitKey)) {
                         haveChecked.push(splitKey)
                         canContainCount++;
@@ -39,6 +37,49 @@ function findBagThatContainsMe(name) {
     return;
 }
 
+function partTwo(pathString) {
+    const input = fs.readFileSync(path.join(__dirname, pathString))
+    const rules = input.toString().trimEnd().split(new RegExp(/\n/, "g"))
+
+    const bagInfo = {}
+
+    rules.forEach((r) => {
+        const splitRule = r.split("contain")
+        const ruleKey = splitRule[0].split(" ").splice(0,2).join(" ");
+        const ruleValues = splitRule[1].split(',');
+
+        ruleValues.forEach((v) => {
+            const amount = v.match(/[0-9]/)
+            if(amount) {
+                const bagName = v.split(" ").splice(2, 2).join(" ")
+                if(!bagInfo[ruleKey]) bagInfo[ruleKey] = {}
+                bagInfo[ruleKey][bagName] = amount[0]
+            }
+        })
+
+    })
+
+    let nest = getNestedBagCount('shiny gold', 1, bagInfo)
+
+    return nest
+}
+
+function getNestedBagCount(bagName, amount, bagInfo) {
+
+    if (!bagInfo[bagName]) return amount * 1;
+    let total = 0;
+    for(const [key, value] of Object.entries(bagInfo[bagName])) {
+        total = total + (amount * getNestedBagCount(key, +value, bagInfo))
+    }
+
+    if(bagName !== "shiny gold") total = total + amount
+
+    return total
+}
+
+
+
 module.exports = {
-    PartOne: partOne
+    PartOne: partOne,
+    PartTwo: partTwo
 }
--- years/2020/days/day 7/test/day7.test.js
+++ years/2020/days/day 7/test/day7.test.js
@@ -5,5 +5,8 @@ describe("AoC day 7", () => {
     test('Part One', () => {
         expect(parts.PartOne("/test/input.txt")).toBe(4);
     })
+    test('Part Two', () => {
+        expect(parts.PartTwo("/test/input.txt")).toBe(32)
+    })
 })
 
